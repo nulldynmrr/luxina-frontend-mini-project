637c31ce64abef006640863db48fafcaae6b078c
nulldynmrr <dinarmuhammadakbar04@gmail.com>
2025-07-05 13:18:06 +0700

    hapus import React

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
 export default function Index() {
   const router = useRouter();
