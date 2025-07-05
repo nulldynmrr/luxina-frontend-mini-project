0ed0974ae56bdab1d2aff6b75f168e6e458889e8
nulldynmrr <dinarmuhammadakbar04@gmail.com>
2025-07-05 13:21:09 +0700

    refactor: update CardMovie menambahkan parameter poster_path and release_date

--- src/components/CardMovie/page.jsx
+++ src/components/CardMovie/page.jsx
@@ -3,21 +3,35 @@
 import React from "react";
 import Image from "next/image";
 import { FaStar } from "react-icons/fa";
+import { useRouter } from "next/navigation";
 
-const CardMovie = ({ image, title, year, rating }) => {
+const apiImg = process.env.NEXT_PUBLIC_BASEIMGURL;
+
+const CardMovie = ({ poster_path, title, release_date, vote_average, id }) => {
+  const router = useRouter();
   return (
-    <div className="relative h-[250px] rounded-xl overflow-hidden bg-[#2B2B2B]">
-      <Image src={image} alt={title} fill className="object-cover rounded-xl" />
+    <div
+      className="relative h-[250px] rounded-xl overflow-hidden bg-[#2B2B2B] cursor-pointer"
+      onClick={() => router.push(`/order?movieId=${id}`)}
+    >
+      <Image
+        src={`${apiImg}${poster_path}`}
+        alt={title}
+        fill
+        className="object-cover rounded-xl"
+      />
 
       <div className="absolute bottom-0 top-0 left-0 right-0 bg-gradient-to-t from-black via-black/40 to-transparent z-10"></div>
 
       <div className="absolute bottom-2 left-2 right-2 z-20 text-white text-sm">
         <h4 className="font-semibold">{title}</h4>
         <div className="flex justify-between text-xs text-[#B0B0B0] mt-1">
-          <span>{year}</span>
+          <span>{release_date ? release_date.slice(0, 4) : "-"}</span>
           <span className="flex items-center gap-1">
             <FaStar className="text-yellow-400" size={12} />
-            {rating}
+            {vote_average !== undefined && vote_average !== null
+              ? vote_average.toString().slice(0, 3)
+              : "-"}
           </span>
         </div>
       </div>
